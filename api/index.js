27866da42a01b2df6300c9c4e5df1d15054e3e42
Sanju <bm.madhubalakrishna@gmail.com>
2025-03-29 21:46:13 +0000

    db connected

--- api/index.js
+++ api/index.js
@@ -1,6 +1,11 @@
 import express from "express";
+import connectDb from "./config/connectDb.js";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
+
+connectDb();
 
 const port = process.env.port || 4000;
 app.listen(port, () => {
